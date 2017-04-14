926351a7e10d5811ba8f77cbd9c29e11dc798bf1
Alexey Stulikov <o1h@ya.ru>
2017-04-14 17:57:30 +0400

    trying to fix bug

--- PPbids/js/script.js
+++ PPbids/js/script.js
@@ -1,19 +1,20 @@
 var limit = 10
-var url = '://prvtparty.com/api/v1/venues'
-url = './data.json'
+var url1 = '://prvtparty.com/api/v1/venues'
 var debug = true
+
+var t = -1;
 
 function get_count() {
 	$.ajax({
 		type: 'GET',
-		url: url,
+		url: url1,
 		dataType: "json"
 	}).done( function(data) {
 		if (debug) { console.log("Count loaded from " + url) }
-		return toInt(data.total_count)
+		t = toInt(data.total_count)
 	}).fail(function() {
 		if (debug) { console.log("Failed remote file download") }
-		return -1
+		t = -1
 	})
 }
 
@@ -29,6 +30,6 @@ function render_count(counter) {
 
 $(document).ready(function() {
 
-	setInterval(render_count(get_count()), 1000)
+	setInterval(function () { get_count(); render_count(t); }, 1000)
 
 })
