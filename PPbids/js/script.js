c3e5a80ad29a0f3704db60a5ebe41e05a7b7a1a4
Alexey Stulikov <o1h@ya.ru>
2017-04-14 18:08:59 +0400

    panic fixing

--- PPbids/js/script.js
+++ PPbids/js/script.js
@@ -1,35 +1,19 @@
 var limit = 10
 var url1 = '://prvtparty.com/api/v1/venues'
-var debug = true
 
-var t = -1;
+$(document).ready(function() {
 
-function get_count() {
 	$.ajax({
 		type: 'GET',
 		url: url1,
 		dataType: "json"
 	}).done( function(data) {
-		if (debug) { console.log("Count loaded from " + url) }
-		t = toInt(data.total_count)
-	}).fail(function() {
-		if (debug) { console.log("Failed remote file download") }
-		t = -1
-	})
-}
-
-function render_count(counter) {
-	if(counter != -1) {
+		counter = data.total_count;
 		$(".counter_number").each(function() {
-			$(this).html(counter)
-		})
-		$(".counter_rest > span").html(limit-counter)
-	}
-}
-
-
-$(document).ready(function() {
-
-	setInterval(function () { get_count(); render_count(t); }, 1000)
-
-})
+			$(this).html(counter);
+		});
+		$(".counter_rest > span").html(limit-counter);
+	}).fail(function() {
+		console.log("Failed remote file download");
+	});
+});
